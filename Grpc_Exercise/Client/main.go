f1d08c0b059556406438b97ad02961ce1f1d1772
Md <mohammedsameer772002@Gmail.com>
2023-02-14 01:14:16 +0530

    review changes made

--- Grpc_Exercise/Client/main.go
+++ Grpc_Exercise/Client/main.go
@@ -9,46 +9,51 @@ import (
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	Conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
+		return 
 	}
-	defer conn.Close()
+	defer Conn.Close()
 
-	client := employee.NewEmployeeServiceClient(conn)
+	client := employee.NewEmployeeServiceClient(Conn)
 
 	//Creating(C)
 
-	resp1, err := client.CreateEmployee(context.Background(), &employee.CreateEmployeeRequest{EmployeeName: "Sameer",ManagerName: "Teja",Department: "Coding"})
+	resp1, err := client.CreateEmployee(context.Background(), &employee.CreateEmployeeRequest{EmployeeName: "Sameer",EmployeeEmail: "sameer1@gmail.com",ManagerName: "Sai",ManagerEmail: "sai1@gmail.com",Department: "Coding"})
 	if err != nil {
 		log.Fatalf("Failed to create employee: %v", err)
+		return 
 	}
 
-	log.Printf("Employee created successfully with ID: %s", resp1.GetMessage())
+	log.Println("Employee Created Successfully : ", resp1.GetEmployee())
 
 	//Reading(R)
 
 	resp2,err := client.ReadEmployee(context.Background(),&employee.ReadEmployeeRequest{EmployeeId: 2})
 	if err!=nil{
 		log.Fatalf("Failed to read employee: %v",err)
+		return 
 	}
 
 	log.Println("Employee Details : ",resp2.GetEmployee())
 
 	//Updating(U)
 
-	resp3,err := client.UpdateEmployee(context.Background(),&employee.UpdateEmployeeRequest{EmployeeName: "Sameer",ManagerName: "Sai"})
+	resp3,err := client.UpdateEmployee(context.Background(),&employee.UpdateEmployeeRequest{EmployeeEmail: "sameer1@gmail.com",ManagerName: "Teja",ManagerEmail: "teja1@gmail.com"})
 	if err!=nil{
 		log.Fatalf("Failed to Update employee :%v",err)
+		return 
 	}
 
-	log.Printf(resp3.GetMessage())
+	log.Println("Updated Employee : ",resp3.GetEmployee())
 
 	//Delete(D)
 
-	resp4,err := client.DeleteEmployee(context.Background(),&employee.DeleteEmployeeRequest{EmployeeName: "Teja"})
+	resp4,err := client.DeleteEmployee(context.Background(),&employee.DeleteEmployeeRequest{EmployeeEmail: "teja1@gmail.com"})
 	if err!=nil{
 		log.Fatalf("Failed to Delete employee :%v",err)
+		return 
 	}
 
 	log.Printf(resp4.GetMessage())
--- Grpc_Exercise/Server/main.go
+++ Grpc_Exercise/Server/main.go
@@ -20,7 +20,8 @@ var (
 
 type Employee struct{
 	gorm.Model
-	EmpName string `gorm:"not null;unique"`
+	EmpName string `gorm:"not null"`
+	EmpEmail string `gorm:"not null;unique"`
 	DepartmentId uint  `gorm:"not null"`
 	Department Department  `gorm:"not null"`
 	ManagerId *uint  
@@ -45,30 +46,52 @@ func (s *employeeServiceServer) CreateEmployee(ctx context.Context,req *employee
 	defer db.Close()
 
 	var dept Department
-	db.FirstOrCreate(&dept,Department{Name: req.GetDepartment()})
+	if err1 := db.FirstOrCreate(&dept,Department{Name: req.GetDepartment()}).Error; err1!=nil{
+		return nil,fmt.Errorf("Error occured during reading or creating a department")
+	}
 
-	var emp Employee
-	db.FirstOrCreate(&emp,Employee{EmpName: req.GetManagerName()})
+	var man Employee
+	if err2 := db.FirstOrCreate(&man,Employee{EmpName: req.GetManagerName(),EmpEmail: req.GetManagerEmail()}).Error; err2!=nil{
+		return nil,fmt.Errorf("Error occured during reading or creating manager")
+	}
 
-	if emp.DepartmentId != 0 && emp.DepartmentId != dept.ID {
+	if man.DepartmentId != 0 && man.DepartmentId != dept.ID {
         return nil,status.Errorf(codes.FailedPrecondition,"Employee and Manager Department Should match")
     }
-    if emp.DepartmentId == 0 {
-        emp.DepartmentId = dept.ID
-        db.Save(&emp)
+    if man.DepartmentId == 0 {
+        man.DepartmentId = dept.ID
+        db.Save(&man)
     }
 	
 	employeeStruct := &Employee{
 		EmpName : req.GetEmployeeName(),
+		EmpEmail: req.GetEmployeeEmail(),
 		DepartmentId: dept.ID,
-		ManagerId: &emp.ID,
+		ManagerId: &man.ID,
 	}
 
 	if err := db.Create(employeeStruct).Error; err!=nil{
 		return nil,status.Errorf(codes.Internal,"Creation of employee failed")
 	}
+
+	var emp Employee
+	db.Model(&Employee{}).Where("emp_email=?",req.EmployeeEmail).Find(&emp)
+
+
 	resp := &employee.CreateEmployeeResponse{
-		Message: fmt.Sprintf("Employee created successfully with Id : %d",employeeStruct.ID),
+		Employee: &employee.Employee{
+			Id: uint32(emp.ID),
+			Name: emp.EmpName,
+			Email: emp.EmpEmail,
+			DepartmentId: uint32(emp.DepartmentId),
+			Department: &employee.Department{
+				Name: emp.Department.Name,
+			},
+			ManagerId: uint32(*emp.ManagerId),
+			Manager: &employee.Employee{
+				Name: emp.Manager.EmpName,
+			},
+		},
 	}
 
 	return resp,nil
@@ -82,16 +105,19 @@ func (s *employeeServiceServer) ReadEmployee(ctx context.Context,req *employee.R
 	defer db.Close()
 
 	var emp Employee
-	db.Model(&Employee{}).Where("id=?",req.GetEmployeeId()).Find(&emp)
+	result := db.Model(&Employee{}).Where("id=?",req.GetEmployeeId()).Find(&emp)
 
-	if emp==(Employee{}){
-		return nil, status.Errorf(codes.NotFound, "Employee with ID %d not found", req.GetEmployeeId())
+	if result.Error != nil{
+		if result.RecordNotFound()==true{
+			return nil,status.Errorf(codes.NotFound,"Employee doesn't exist in the employee table")
+		}
 	}
 
 	resp :=  &employee.ReadEmployeeResponse{
 		Employee: &employee.Employee{
 			Id: uint32(emp.ID),
 			Name: emp.EmpName,
+			Email: emp.EmpEmail,
 			DepartmentId: uint32(emp.DepartmentId),
 		},
 	}
@@ -110,15 +136,19 @@ func (s *employeeServiceServer) UpdateEmployee(ctx context.Context,req *employee
 	defer db.Close()
 
 	var emp1 Employee
-	db.Model(&Employee{}).Where("emp_name=?",req.GetEmployeeName()).Find(&emp1)
+	result := db.Model(&Employee{}).Where("emp_email=?",req.GetEmployeeEmail()).Find(&emp1)
 
-	if emp1==(Employee{}){
-		return nil,status.Errorf(codes.NotFound,"Employee doesn't exist in the employee table")
+	if result.Error != nil{
+		if result.RecordNotFound()==true{
+			return nil,status.Errorf(codes.NotFound,"Employee doesn't exist in the employee table")
+		}
 	}
 
 	var emp2 Employee
 
-	db.FirstOrCreate(&emp2,Employee{EmpName:req.GetManagerName()})
+	if err1 := db.FirstOrCreate(&emp2,Employee{EmpName:req.GetManagerName(),EmpEmail: req.GetManagerEmail()}); err1!=nil{
+		return nil,fmt.Errorf("Error while reading or creating manager")
+	}
 
 	if emp2.DepartmentId!=0 && emp1.DepartmentId!=emp2.DepartmentId{
 		return nil,status.Errorf(codes.FailedPrecondition,"Employee and Manager Departments should match")
@@ -133,7 +163,13 @@ func (s *employeeServiceServer) UpdateEmployee(ctx context.Context,req *employee
 	db.Save(&emp1)
 
 	resp := &employee.UpdateEmployeeResponse{
-		Message: fmt.Sprintf("Employee with Id : %d updated successfully",emp1.ID),
+		Employee: &employee.Employee{
+			Id: uint32(emp1.ID),
+			Name: emp1.EmpName,
+			Email: emp1.EmpEmail,
+			ManagerId: uint32(*emp1.ManagerId),
+			DepartmentId: uint32(emp1.DepartmentId),
+		},
 	}
 
 	return resp,nil
@@ -148,14 +184,24 @@ func (s *employeeServiceServer) DeleteEmployee(ctx context.Context,req *employee
 	defer db.Close()
 
 	var emp Employee
-	db.Model(&Employee{}).Where("emp_name=?",req.GetEmployeeName()).Find(&emp)
+	result := db.Model(&Employee{}).Where("emp_email=?",req.GetEmployeeEmail()).Find(&emp)
 
-	db.Model(&Employee{}).Where("manager_id = ?", emp.ID).Updates(map[string]interface{}{"manager_id": nil})
+	if result.Error != nil{
+		if result.RecordNotFound()==true{
+			return nil,status.Errorf(codes.NotFound,"Employee doesn't exist in the employee table")
+		}
+	}
+
+	if err := db.Model(&Employee{}).Where("manager_id = ?", emp.ID).Updates(map[string]interface{}{"manager_id": nil}).Error; err!=nil{
+		return nil,fmt.Errorf("Error while making manager_ids nil")
+	}
 	
-	db.Delete(&emp)
+	if err2 := db.Delete(&emp).Error; err2!=nil{
+		return nil,fmt.Errorf("Error while deleting an employee")
+	}
 
 	resp := &employee.DeleteEmployeeResponse{
-		Message: fmt.Sprintf("Employee with name :%s deleted successfully",req.GetEmployeeName()),
+		Message: fmt.Sprintf("Employee with email :%s deleted successfully",emp.EmpEmail),
 	}
 
 	return resp,nil
@@ -166,11 +212,13 @@ func main(){
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
+		return 
 	}
 	s := grpc.NewServer()
 	employee.RegisterEmployeeServiceServer(s,&employeeServiceServer{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+		return 
 	}
 }
